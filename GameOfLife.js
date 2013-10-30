8d3b7594169f3efa146cedac0ead826ca28afbbd
Rafael Cosman <rafaelc@stanford.edu>
2013-10-30 09:33:48 -0700

    compiled coffeescript to javascript

--- GameOfLife.js
+++ GameOfLife.js
@@ -120,7 +120,7 @@ Author Rafael Cosman
 
   ages = makeNewGrid();
 
-  console.log(ages);
+  console.log(ages[0]);
 
   rules = [[false, false, false, true, false, false, false, false], [false, false, true, true, false, false, false, false]];
 
