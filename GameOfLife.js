b009d348e21fac5b187745a9276a1fa258a4c03a
Rafael Cosman <rafaelc@stanford.edu>
2013-11-01 15:47:14 -0700

    compiled coffeescript to javascript

--- GameOfLife.js
+++ GameOfLife.js
@@ -197,7 +197,7 @@ Maddy approved.
           if (rules[x][y]) {
             context.fillStyle = "rgba(0, 0, 0, .5)";
           } else {
-            context.fillStyle = "rgba(255, 255, 255, .3)";
+            context.fillStyle = "rgba(255, 255, 255, .6)";
           }
           _results1.push(context.fillRect(buttonWidth * x, buttonHeight * y, buttonWidth, buttonHeight));
         }
