617db63e9b40c115dd068b38a3b15c6b331d9314
Rafael Cosman <rafaelc@stanford.edu>
2013-11-05 08:29:25 -0800

    compiled coffeescript to javascript

--- GameOfLife.js
+++ GameOfLife.js
@@ -296,7 +296,7 @@ This code is Maddy approved.
     return --mouseDownCount;
   };
 
-  $("#document.body").mousemove(function(event) {
+  $("document.body").mousemove(function(event) {
     var d, gridX, gridY, x, y, _i, _j, _k, _ref, _ref1, _ref2, _ref3, _ref4, _ref5, _results;
     d = 2;
     if (mouseDown[0]) {
